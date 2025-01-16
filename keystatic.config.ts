6af93b8bc4170bbae352247c6fcf80f397d45d87
@Neal <s80793@gmail.com>
2025-01-16 17:24:01 +0800

    update config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,4 +1,4 @@
-import { collection, config, fields } from '@keystatic/core';
+import { collection, config, fields, singleton } from "@keystatic/core";
 
 export default config({
   storage: {
@@ -9,9 +9,26 @@ export default config({
       label: "Posts",
       slugField: "title",
       path: "/content/posts/*",
+      entryLayout: "content",
       format: { contentField: "content" },
       schema: {
         title: fields.slug({ name: { label: "Title" } }),
+        description: fields.text({ label: "Description" }),
+        publishedDate: fields.date({ label: "Published date" }),
+        content: fields.markdoc({ label: "Content" }),
+      },
+    }),
+  },
+  singletons: {
+    settings: singleton({
+      label: "Settings",
+      path: "content/posts/",
+      entryLayout: "content",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        description: fields.text({ label: "Description" }),
+        publishedDate: fields.date({ label: "Published date" }),
         content: fields.markdoc({ label: "Content" }),
       },
     }),
